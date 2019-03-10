7ec1e939d60048b56a405d73ed6b58c92cb4c703
qwn <atharvak1910@gmail.com>
2019-03-10 21:41:04 +0530

    Default to grub boot

--- Kernel/kentry.asm
+++ Kernel/kentry.asm
@@ -1,5 +1,23 @@
+; Declare constants for the multiboot header.
+MBALIGN  equ  1 << 0            ; align loaded modules on page boundaries
+MEMINFO  equ  1 << 1            ; provide memory map
+FLAGS    equ  MBALIGN | MEMINFO ; this is the Multiboot 'flag' field
+MAGIC    equ  0x1BADB002        ; 'magic number' lets bootloader find the header
+CHECKSUM equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
+ 
+; Declare a multiboot header that marks the program as a kernel. These are magic
+; values that are documented in the multiboot standard. The bootloader will
+; search for this signature in the first 8 KiB of the kernel file, aligned at a
+; 32-bit boundary. The signature is in its own section so the header can be
+; forced to be within the first 8 KiB of the kernel file.
+section .multiboot
+align 4
+	dd MAGIC
+	dd FLAGS
+	dd CHECKSUM
+
+section .text
     bits 32
-    section .text
     global __start
     extern kernel_main
 __start:
@@ -10,12 +28,8 @@ __start:
     ;mov     ds, eax
     ;mov     ss, eax
     ;mov     es, eax
-    cli
-    pop     ax
-    mov     ds, ax
-    mov     ss, ax
-    mov     es, ax
     mov     sp, 07c00h
+    cli
     call    kernel_main
     hlt
     hlt
@@ -37,7 +51,9 @@ PPrintString:
     popa
     ret
 
-    section .data
+    %if 0
+section .data
 WelcomeString: db "Welcome to the Kernel",0
 TestSting: db "After read",0
+    %endif
 
--- Makefile
+++ Makefile
@@ -5,18 +5,19 @@ debug:clean all
 	qemu-system-x86_64 -s -S -drive file=$(output),media=disk,format=raw
 
 release:clean all
+	qemu-system-x86_64 -cdrom Build/KulkarNIX.iso
 	qemu-system-x86_64 -drive file=$(output),index=0,media=disk,format=raw
 
 build:clean all
 
-all: FirstStage SecondStage kernel WriteImage
+all: kernel WriteImage
 
 FirstStage:
 	$(MAKE) -C Bootloader/FirstStage
 SecondStage:
 	$(MAKE) -C Bootloader/SecondStage
 kernel:
-	$(MAKE) -C Kernel/ all
+	$(MAKE) -C Kernel/
 
 WriteImage:
 	$(MAKE) -C Build/ 
