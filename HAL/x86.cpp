0821060429bea5899d6f36f6351f52d1ec2c98c2
qwn <atharvak1910@gmail.com>
2020-03-17 22:23:56 +0530

    Check if CPUID exists for the CPU

--- HAL/x86.cpp
+++ HAL/x86.cpp
@@ -12,4 +12,13 @@ void DisableInterrupts()
   asm volatile("cli");
 }
 
+BOOL CheckIfApicExists()
+{
+    uint32_t edx = 0, a = 1;
+    asm volatile("cpuid": "=d" (edx): "a" (a));
+    if (edx & 0x200)
+        return true;
+    return false;
+
+}
 } // namespace x86
