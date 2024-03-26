4a083d3122c308481e662ca91eab4a1d6a11e296
Atharva Kulkarni <atharvak1910@gmail.com>
2024-03-26 21:27:41 +0530

    Finish init. Not  tested

--- Kernel/KPIC.c
+++ Kernel/KPIC.c
@@ -1,5 +1,4 @@
 #include "KPIC.h"
-#include <x86.h>
 
 #define PIC1      0x20
 #define PIC1_CMD  PIC1
@@ -9,26 +8,64 @@
 #define PIC2_CMD  PIC2
 #define PIC2_DATA (PIC2 + 1)
 
-typedef struct _PIC {
-}PIC, *PPIC;
-
-static PIC gPIC = {};
-
 void InitPIC()
 {
     /*
      * ICW1: ((1 << 4) | 1)
      * IC4  : 1 : ICW4 is needed
      * SNGL : 0 : Cascade mode
-     * ADI  : 0 : Vector address interval is 4
+     * ADI  : 0 : Vector address interval is 8
      * LTIM : 0 : Level Triggered
      * RESV : 1 : Reserved
      * A5 - A7 : 0 : Not applicable
-     * A0   : 0 : Reserved
      */
     outb(PIC1_CMD, ((1 << 4) | 1));
     outb(PIC2_CMD, ((1 << 4) | 1));
 
+    /*
+     * ICW2:
+     * Since ADI in ICW1 is 0 i.e address interval is 8
+     * A8 - A10 : 0 : Ignored
+     * A11 - A15 : Offset into Interrupt Vector
+     * Master will start at Interrupt vector 32 (0x20)
+     * Slave will start at Interrupt vector 40 (0x28)
+     */
+    outb(PIC1_DATA, 0x20);
+    outb(PIC2_DATA, 0x28);
+
+    /*
+     * ICW3:
+     * Master :
+     * We will setup so that Slave is on IRQ2
+     * S0 - S7 : Bits indicate which IR has a slave
+     *
+     * Slave  :
+     * Will have the ID 0x2
+     */
+    outb(PIC1_DATA, (1 << 2));
+    outb(PIC2_DATA, (1 << 1));
+
+    /*
+     * ICW4:
+     * Mode : 1 : 8086 Mode
+     * D1 - D7 : 0 : Ignored (There is some meanings to this but we
+     *                        ignore it for now)
+     */
+    outb(PIC1_DATA, 1);
+    outb(PIC2_DATA, 1);
+
+}
+
+void
+DisablePIC()
+{
+    outb(PIC1_DATA, 0xff);
+    outb(PIC2_DATA, 0xff);
+}
+
+void
+MaskIntr(uint32 intrLine)
+{
 
 }
 
--- Kernel/KPIC.h
+++ Kernel/KPIC.h
@@ -1,13 +1,40 @@
 #pragma once
+#include <x86.h>
 #include "typedefs.h"
 
 /*
 InitPIC
 
 Description:
+  Performs intialization sequence for 8259A
+  The master interrupts are mapped to (0x20 - 0x27)
+  The slave interrupts are mapped to (0x28 - 0x2F)
   Intializes the master and slave 8259A PIC in cascade mode
 
 Arguments:
   None
 */
 void InitPIC();
+
+/*
+DisablePIC
+
+Description:
+  Disables the PIC. All interrupts are masked
+
+Arguments:
+  None
+*/
+void DisablePIC();
+
+
+/*
+MaskIntr
+
+Description:
+  Masks the interrupt line
+
+Arguments:
+  intrLine : Interrupt line to be mased
+*/
+void MaskIntr(uint32 intrLine);
