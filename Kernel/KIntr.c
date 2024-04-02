a493538a6ccc027cc8653e72646ae57e0414b016
Atharva Kulkarni <atharvak1910@gmail.com>
2024-04-02 21:22:22 +0530

    Start adding interrupts?

--- Kernel/KIntr.c
+++ Kernel/KIntr.c
@@ -1,13 +1,45 @@
 #include "KIntr.h"
 
-struct _IDTR {
+typedef struct _IDTR {
     uint16_t IDTRSize;
     uint32_t pIDTR;
-}__attribute__((packed));
+}__attribute__((packed)) IDTR;
+IDTR idtr;
 
-typedef struct _IDTR IDTR;
+typedef struct _INTR_GATE {
+    uint16_t offset1;
+    uint16_t selector;
+    union {
+        uint8_t resv1;
+        uint8_t resv2:5;
+        uint8_t dpl:1;
+        uint8_t p:1;
+    }u;
+    uint16_t offset2;
+}INTR_GATE, *PINTR_GATE;
+INTR_GATE InterruptTable[256];
+
+
+void
+AddInterrupts(uint8_t idx, uint32_t address)
+{
+    PINTR_GATE pGate = &InterruptTable[idx];
+    pGate->offset1   = address & 0xFF;
+    pGate->offset2   = (address >> 16) & 0xFF;
+    pGate->u.resv1   = 0;
+    pGate->u.resv2   = 0xe;
+    pGate->u.p       = 1;
+    pGate->u.dpl     = 1;
+    pGate->selector  = 0;
+}
 
 void
 InitInterrupts()
 {
+    idtr.pIDTR = (uint32_t)&InterruptTable;
+    idtr.IDTRSize = sizeof(InterruptTable);
+
+    /* Setup Exceptions and interrupts */
+
+    /* Load IDTR */
 }
