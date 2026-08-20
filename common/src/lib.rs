3a0a76064954ca15e539b187490ff1c3c85434e2
Atharva Kulkarni <atharvak1910@gmail.com>
2026-08-20 14:40:16 +0530

    Update lib.rs

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,9 +1,14 @@
 #![no_std]
 pub mod paging;
+use r_efi::efi::MemoryDescriptor;
 extern crate alloc;
 use alloc::vec;
 
+const MMAP_BUFFER_SIZE: usize = paging::PAGE_SIZE - (core::mem::size_of::<usize>() * 2);
+
+#[repr(C)]
 pub struct KernelArgs {
-    pub key: usize,
-    pub buffer: vec::Vec<u8>,
+    pub desc_size: usize,
+    pub mem_map_size: usize,
+    pub buffer: [u8; MMAP_BUFFER_SIZE],
 }
