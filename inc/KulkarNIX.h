273b260eaf2da9cb2708ac54b993cbd1191b8b12
Atharva Kulkarni <atharvak1910@gmail.com>
2026-09-15 12:31:51 +0530

    Update KulkarNIX.h

--- inc/KulkarNIX.h
+++ inc/KulkarNIX.h
@@ -1,12 +1,21 @@
-#ifndef _KULKARNIX_H
-#define _KULKARNIX_H
+#pragma once
+#include <stdint.h>
 /////////////////////////////////////////////////////////////////////////////////
 // This file contains all common MACROS and defines needed for all the modules //
 /////////////////////////////////////////////////////////////////////////////////
 
-// Work around compile warnings
-#define UNREFRENCED_PARAMETER(X) X;
-#define KERNEL_START_PADDR 0x100000
-#define KERNEL_START_VADDR 0xfffffa0000000000;
-#define HIGHER_MEMORY_VADDR 0xFFFF800000000000
-#endif
+constexpr uint64_t KERNEL_START_PADDR = 0x100000;
+constexpr uint64_t KERNEL_START_VADDR = 0xfffffa0000000000;
+constexpr uint64_t HIGHER_MEMORY_VADDR = 0xFFFF800000000000;
+constexpr uint16_t PAGE_SIZE = 4096;
+
+template <typename T>
+constexpr T CEILING(T x, T y) {
+    return ((x + (y -1))/y);
+}
+
+template <typename T>
+constexpr T PA2VA(T PA)
+{
+    return reinterpret_cast<T>(reinterpret_cast<uint64_t>(PA) + HIGHER_MEMORY_VADDR);
+}
