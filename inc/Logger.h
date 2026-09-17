f4e3b1f828b34c1a82b0671d06aad93651c07122
Atharva Kulkarni <atharvak1910@gmail.com>
2026-09-17 15:28:34 +0530

    Update Logger.h

--- inc/Logger.h
+++ inc/Logger.h
@@ -1,13 +1,16 @@
 #pragma once
-#include "serial_port.h"
-#include "slice.h"
+#include "SerialPort.h"
+#include "Slice.h"
 #include <stdarg.h>
 
 class Logger {
   private:
-    SerialPort sp;
-    inline static char m_buffer[1024];
     inline static const char digits[] = "0123456789abcdef";
+    inline static char m_buffer[1024];
+    inline static uint32_t m_idx{0};
+
+    Slice<char> buf{m_buffer, sizeof(m_buffer)};
+    SerialPort sp;
 
     size_t itoa(uint64_t number, Slice<char> s, int radix) {
         size_t idx = 0;
@@ -26,55 +29,53 @@ class Logger {
         return i;
     }
 
-    bool vsprintf(Slice<char>& s, const char* str, va_list args) {
-        if (s.size() == 0 || str == nullptr)
-            return false;
+    // TODO
+    void write(const char c) { m_buffer[m_idx++] = c; }
 
-        const char* itr = str;
-        size_t idx = 0;
-
-        while (*itr != '\0' && idx < s.size()) {
-            if (*itr != '%') {
-                s[idx++] = *itr;
-                itr++;
-                continue;
-            } else {
-                itr++;
-            }
-
-            switch (*itr) {
-            case 'd': {
-                idx += itoa(va_arg(args, uint64_t), s.sub_slice(idx, s.size()), 10);
-            } break;
-
-            case 'x': {
-                idx += itoa(va_arg(args, uint64_t), s.sub_slice(idx, s.size()), 16);
-            } break;
-
-            default:
-                return false;
-            }
-
-            itr++;
+    void write(const char* str) {
+        while (*str != '\0') {
+            m_buffer[m_idx++] = *str;
         }
+    }
 
-        if (idx < s.size() && s.size() - idx >= 2) {
-            s[idx++] = '\n';
-            s[idx] = '\0';
+    void write(const bool b) {
+        if (b)
+            write("true");
+        else
+            write("false");
+    }
+
+    void write(const int64_t i) {
+    }
+
+    void write(const uint64_t i) {
+
+    }
+
+    template <typename T> void write(T x) {
+        if constexpr (T(-1) < T(0)) {
+            write(static_cast<int64_t>(x));
         } else {
-            return false;
+            write(static_cast<uint64_t>(x));
         }
-        return true;
     }
 
   public:
-    void print(const char* str, ...) {
-        va_list valist;
-        auto buf = Slice(m_buffer, sizeof(m_buffer));
-        va_start(valist, str);
-        if (vsprintf(buf, str, valist)) {
-            sp.write(m_buffer);
+    Logger() {}
+
+    template <typename First, typename... Rest>
+    void fprint(const char* str, const First& first, const Rest&... rest) {
+        while (*str != '\0') {
+            if (*str == '{' and *(str + 1) == '}') {
+                write(first);
+                str += 2;
+                fprint(str, rest...);
+            } else
+                write(*str);
         }
-        va_end(valist);
+    }
+
+    template <typename... Args> void print(const Args&... args) {
+        (write(args), ...);
     }
 };
